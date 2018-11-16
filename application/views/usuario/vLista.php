101fef1c155ff4bbcdb82b19142f7dc29065ca3b
SusanQuintanilla16 <susan.quintanilla@gmail.com>
2018-11-15 19:18:52 -0600

    vLista

---
+++ application/views/usuario/vLista.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h2>Usuarios registrados</h2>
+<?php
+if($listaUsuarios==null)
+{
+	echo "<h3>No hay usuarios registrados en el sistema</h3>"
+}
+else
+{
+	?>
+<table>
+	<thead>
+		<th>Usuario</th>
+		<th>Eliminar</th>
+	</thead>	
+	<tbody>
+		<?php 
+			foreach ($listaU as $row) {
+			?>
+			<tr>
+				<td><?php echo $row->Usuario;?></td>
+				<td><a href="<?php echo base_url();?>CUsuario/delete/<?php echo $row->Id;?>" onClick="return confirm('¿Está seguro de eliminar usuario?');">Eliminar</a></td>
+			</tr>
+			<?php
+			}
+		?>
+	</tbody>
+<?php
+}
+?>
+</body>
+</html>
